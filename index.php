72c95ad77d31cd7b3dc90504f377c3a0837ecb36
JCORTABRAZO <jc-2107@hotmail.com>
2025-03-17 22:56:53 -0500

    inicio de proyecto

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maqueta web</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="">Inicio</a>
+            <a href="">Nosotros</a>
+            <a href="">Proyectos</a>
+            <a href="">Contacto</a>
+        </nav>
+        <div class="header_contenido">
+            <img src="https://picsum.photos/id/684" alt="">
+        </div>
+    </header>    
+</body>
+</html>
